d3460c6f0f4569ff62a2b2f36a659350eae93945
sinsau182 <singhsaurav182001@gmail.com>
2024-10-07 16:05:40 +0530

    tech

--- src/pages/about.js
+++ src/pages/about.js
@@ -81,17 +81,17 @@ const about = () => {
                                 xl:col-span-8 xl:flex-row xl:items-center md:order-3'>
                     <div className='flex flex-col items-end justify-center xl:items-center'>
                         <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
-                            <AnimatedNumbers number={50} />+
+                            <AnimatedNumbers number={30} />+
                         </span>
                         <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75 
                         xl:text-center md:text-lg sm:text-base xs:text-sm'>
-                        satisfied clients
+                        Known Tech Stacks
                         </h2>
                     </div>
 
                     <div className='flex flex-col items-end justify-center xl:items-center'>
                         <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
-                            <AnimatedNumbers number={40} />+
+                            <AnimatedNumbers number={20} />+
                         </span>
                         <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75
                         xl:text-center md:text-lg sm:text-base xs:text-sm'>
