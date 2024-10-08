4221eb3a8008293209168b4cad209b0795a43381
sinsau182 <singhsaurav182001@gmail.com>
2024-10-08 19:33:04 +0530

    code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,7 +15,7 @@ const Footer = () => {
                 ' target={"_blank"}
                 >SinghSaurav</Link>
             </div>
-            <Link href="https://wa.me/8825398529?text=Hello%2C%20I%27m%20interested%20in%20your%20services
+            <Link href="https://wa.me/+91-8825398529?text=Hello%2C%20I%27m%20interested%20in%20your%20services
 " target={"_blank"}
             className='underline
                 underline-offset-2'
