742c6a2ad7b0cd69853073ebcceab7ab381ab6a9
sinsau182 <singhsaurav182001@gmail.com>
2025-01-01 03:08:24 +0530

    year change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
     <footer className='w-full border-t-2 border-solid border-dark
     font-medium text-lg dark:text-light dark:border-light sm:text-base'>
         <Layout classname='py-8 flex items-center justify-between lg:flex-col lg:py-6'>
-            <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
+            <span>{"2025"} &copy; All Rights Reserved.</span>
             <div className='flex items-center lg:py-2'>
             Build With <span className='text-primary dark:text-primaryDark text-2xl px-1'>&#9825;</span>
                 by&nbsp; <Link href="https://www.linkedin.com/in/saurav-singh-15320a218/" className='underline
